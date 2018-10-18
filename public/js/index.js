a0658d43e2f0dc2d4b4feea145009c2315563f47
qronix <qroniix@gmail.com>
2018-10-18 14:17:17 -0400

    Add form for messages and show incoming messages to client

--- public/js/index.js
+++ public/js/index.js
@@ -14,4 +14,19 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
     console.log(message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
+});
+
+$('#message-form').on('submit',function(event){
+    event.preventDefault();
+    socket.emit('createMessage',{
+        from: 'User',
+        text:$('[name=message]').val()
+    },function(data){
+        console.log(data);
+    });
+    $('[name=message]').val('');
 });
