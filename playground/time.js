685d1a119c22185134e3242df032bf4bc45cc64a
qronix <qroniix@gmail.com>
2018-10-20 19:12:19 -0400

    Add formatted time strings to messages

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+
+
+new Date().getTime();
+var someTimestamp = moment().valueOf();
+var date = moment(createdAt);
+var createdAt = 1234;
+
+console.log(date.format('h:mm a'));
+
