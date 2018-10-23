25d01e19c06d8d64abae81b136dc4b0692fc9015
qronix <qroniix@gmail.com>
2018-10-23 15:23:45 -0400

    Add mustache js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -8,27 +8,26 @@ socket.on('disconnect',function(){
     console.log('Disconnected from server!');
 });
 
-// socket.on('newEmail',function(email){
-//     console.log('new email!',email);
-// });
-
 socket.on('newMessage',function(message){
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    $('#messages').append(li);
+    var template =$('#message-template').html();
+    var html = Mustache.render(template,{
+        text:message.text,
+        from:message.from,
+        createdAt:formattedTime
+    });
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage',function(message){
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current location</a>')
-
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href',message.url);
-    li.append(a);
-    $('#messages').append(li);
+    var template = $("#location-message-template").html();
+    var html = Mustache.render(template,{
+        url:message.url,
+        from:message.from,
+        createdAt:formattedTime
+    });
+    $('#messages').append(html);
 });
 
 $('#message-form').on('submit',function(event){
